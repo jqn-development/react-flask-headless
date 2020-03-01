75167bbf4264f3e03a83060340babb9b92b98ff5
jQN <jqn@jQNs-MacBook-Pro.local>
2020-03-01 07:55:34 +0100

    completed basic scrapping app

--- README.md
+++ README.md
@@ -17,4 +17,12 @@ Step 3. Create and activate a new virtual environment `$ mkvirtualenv react_flas
 
 Step 4. Install requirements `$ pip install -r requirements.txt`
 
-Step 5. Run the application `$ export FLASK_APP=run.py` and `$ flask run`
+Step 5. Run the application
+
+- `$ export FLASK_APP=run.py`
+- `$ export FLASK_ENV=development`
+- `$ flask run`
+
+Step 6. Open your browser and point it to this url `http://127.0.0.1:5000/api/v1/soup/getsoup/<add-url-here>`
+
+Example: `http://127.0.0.1:5000/api/v1/soup/getsoup/https://www.ventureharbour.com/10-lead-capture-page-examples-help-capture-leads/`
--- app/services.py
+++ app/services.py
@@ -1,15 +1,16 @@
 # services.py
 from urllib.request import Request, urlopen
 from bs4 import BeautifulSoup
-from flask import jsonify
+from flask import jsonify, request
 
 from app import app
 
-@app.route('/api/v1/soup/getsoup/<url>',  methods=["GET"])
-def getPage(url):
+@app.route('/api/v1/soup/getsoup/<path:subpath>',  methods=["GET"])
+def getPage(subpath):
   try:
+
     # make the request
-    req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+    req = Request(subpath, headers={'User-Agent': 'Mozilla/5.0'})
 
     # query the website and return the html to the variable ‘page’
     page = urlopen(req)
@@ -17,7 +18,15 @@ def getPage(url):
     # parse the html using beautiful soup and store in variable `soup`
     soup = BeautifulSoup(page, 'html.parser')
 
-    return soup.prettify()
+    # initialize an empty list to hold subtitles
+    subtitle_list = []
+
+    for subtitle in soup.find_all('h2'):
+      print(subtitle)
+      print(type(subtitle))
+      subtitle_list.append(str(subtitle))
+
+    return jsonify(subtitles=subtitle_list)
   except Exception as e:
     print(e)
     return jsonify({"error": "Unexpected error"}), 500
